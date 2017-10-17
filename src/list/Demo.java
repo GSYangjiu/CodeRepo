ca47e1989298a7311802616284c9ac2e897c8e6d
Yangyangyang <1094259423@qq.com>
2017-10-17 22:44:55 +0800

    Update：学习ArrayList源码，手动实现ArrayList基本功能

--- src/list/Demo.java
+++ src/list/Demo.java
@@ -13,8 +13,15 @@ public class Demo {
         }};
         System.out.println("移除前list长度："+list.size());
         System.out.println(list.get(1));
-        list.remove(1);
+        list.remove(6);
         System.out.println("移除后list长度："+list.size());
         System.out.println(list.get(1));
+        SxtArrayList a = new SxtArrayList();
+        a.add(1);
+        a.add(2);
+        a.add(3);
+        System.out.println("移除前list长度："+a.size());
+        a.remove(2);
+        System.out.println("移除后list长度："+a.size());
     }
 }
