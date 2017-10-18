5c4bda676c64f400a823db97ab6ea3eaad8d7f0f
Yangyangyang <1094259423@qq.com>
2017-10-19 00:36:30 +0800

    Update：手动实现LinkList基本功能

--- src/list/Demo.java
+++ src/list/Demo.java
@@ -8,20 +8,32 @@ import java.util.List;
  */
 public class Demo {
     public static void main(String[] args) {
-        List<Integer> list = new ArrayList<Integer>() {{
-            add(2);add(3);add(4);add(10);add(15);add(16);add(17);
-        }};
-        System.out.println("移除前list长度："+list.size());
-        System.out.println(list.get(1));
-        list.remove(6);
-        System.out.println("移除后list长度："+list.size());
-        System.out.println(list.get(1));
-        SxtArrayList a = new SxtArrayList();
-        a.add(1);
-        a.add(2);
-        a.add(3);
-        System.out.println("移除前list长度："+a.size());
-        a.remove(2);
-        System.out.println("移除后list长度："+a.size());
+//        List<Integer> list = new ArrayList<Integer>() {{
+//            add(2);add(3);add(4);add(10);add(15);add(16);add(17);
+//        }};
+//        System.out.println("移除前list长度："+list.size());
+//        System.out.println(list.get(1));
+//        list.remove(6);
+//        System.out.println("移除后list长度："+list.size());
+//        System.out.println(list.get(1));
+//        SxtArrayList a = new SxtArrayList();
+//        a.add(1);
+//        a.add(2);
+//        a.add(3);
+//        System.out.println("移除前list长度："+a.size());
+//        a.remove(2);
+//        System.out.println("移除后list长度："+a.size());
+        SxtLinkList linkList = new SxtLinkList();
+        linkList.add("123");
+        linkList.add("456");
+        linkList.add("789");
+        linkList.add("12424");
+        System.out.println(linkList.size());
+        //System.out.println(linkList.get(1));
+        linkList.remove(1);
+        System.out.println(linkList.get(0));
+        System.out.println(linkList.get(1));
+        System.out.println(linkList.get(2));
+        System.out.println(linkList.size());
     }
 }
