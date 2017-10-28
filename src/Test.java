f7f144449b1039b8ddeffe20c0853b07cee9dfd1
yanghaohuan <1094259423@qq.com>
2017-10-28 17:56:58 +0800

    no message

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created by Yang on 2017/10/25 0025.
  */
@@ -5,7 +7,10 @@ public class Test {
     public final int i = 1;
 
     public static void main(String[] args) {
-        Test test = new Test();
-        System.out.println(test.i);
+//        Test test = new Test();
+//        System.out.println(test.i);
+        int[] arr ={5,9,4,326,4,5};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
