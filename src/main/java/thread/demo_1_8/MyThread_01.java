b52c5eaa9165a48142b9d4766e0c61bc077c5e20
Yangyangyang <1094259423@qq.com>
2018-03-28 18:32:49 +0800

    暂停线程

---
+++ src/main/java/thread/demo_1_8/MyThread_01.java
@@ -0,0 +1,24 @@
+package thread.demo_1_8;
+
+/**
+ * Created by Yang.
+ * Email : vincent1094259423@yahoo.com
+ * Date  : 2018-03-28 18:21
+ * Description:
+ */
+public class MyThread_01 extends Thread {
+    private long i = 0;
+
+    public long getI() {
+        return i;
+    }
+
+    public void setI(long i) {
+        this.i = i;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+    }
+}
