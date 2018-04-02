51e0bcb2115edb595b590c60a11afd34b9a7a577
Yangyangyang <1094259423@qq.com>
2018-04-02 09:32:32 +0800

    设计模式：组合模式

---
+++ src/main/java/designPattern/composite/Employee.java
@@ -0,0 +1,43 @@
+package designPattern.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Yang.
+ * Email : vincent1094259423@yahoo.com
+ * Date  : 2018-04-02 09:13
+ * Description:
+ */
+public class Employee {
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    public Employee(String name, String dept, int salary) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+        this.subordinates = new ArrayList<>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates(){
+        return subordinates;
+    }
+
+    @Override
+    public String toString() {
+        return ("Employee :[ Name : " + name
+                + ", dept : " + dept + ", salary :"
+                + salary + " ]");
+    }
+}
